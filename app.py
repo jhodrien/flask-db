8df8ead39c5e56fd11a78988508949ae52823289
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2024-02-28 20:30:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ import psycopg2
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-#cred = DefaultAzureCredential()
-#accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
+cred = DefaultAzureCredential()
+accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
 
 # Combine the token with the connection string from the environment variables added by Service Connector to establish the connection.
-#conn_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
-#conn = psycopg2.connect(conn_string + ' password=' + accessToken.token) 
-
+conn_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+conn = psycopg2.connect(conn_string + ' password=' + accessToken.token) 
 
 app = Flask(__name__)
 
@@ -28,20 +27,20 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
 
-   version = os.environ
-#   with conn.cursor() as curs:
-#      try:
-#         # simple single row system query
-#         curs.execute("SELECT version()")
-#
-#         # returns a single row as a tuple
-#         version = curs.fetchone()
-#
-#      # a more robust way of handling errors
-#      except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-   env = ' '.join(map(str,os.environ))
-   name = request.form.get('name') + " " + version + " " + env
+   version = "unknown"
+   with conn.cursor() as curs:
+      try:
+         # simple single row system query
+         curs.execute("SELECT version()")
+
+         # returns a single row as a tuple
+         version = curs.fetchone()
+
+      # a more robust way of handling errors
+      except (Exception, psycopg2.DatabaseError) as error:
+         print(error)
+
+   name = request.form.get('name') + " " + version
 
    if name:
        print('Request for hello page received with name=%s' % name)
