484a1af8134aeb4ec982b131afc64bd02a7d188f
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2024-02-28 19:53:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
+from azure.identity import DefaultAzureCredential
+import psycopg2
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-app = Flask(__name__)
+cred = DefaultAzureCredential()
+accessToken = cred.get_token('https://ossrdbms-aad.database.windows.net/.default')
 
+# Combine the token with the connection string from the environment variables added by Service Connector to establish the connection.
+conn_string = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+conn = psycopg2.connect(conn_string + ' password=' + accessToken.token) 
+
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -13,12 +22,25 @@ def index():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+   return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+
+   version = "unknown"
+   with conn.cursor() as curs:
+      try:
+         # simple single row system query
+         curs.execute("SELECT version()")
+
+         # returns a single row as a tuple
+         version = curs.fetchone()
+
+      # a more robust way of handling errors
+      except (Exception, psycopg2.DatabaseError) as error:
+         print(error)
+     name = request.form.get('name') + " " + version
 
    if name:
        print('Request for hello page received with name=%s' % name)
